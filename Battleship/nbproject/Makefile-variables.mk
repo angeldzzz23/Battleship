9d38cc396b16ceeed4d2e5f17da3322ca8a759f3
EthanolASD <91338012+EthanolASD@users.noreply.github.com>
2021-09-28 18:11:30 -0700

    Stuff
    Stuff

--- Battleship/nbproject/Makefile-variables.mk
+++ Battleship/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=battleship
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/battleship
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/battleship
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=battleship.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/battleship.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/battleship.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-MacOSX
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX

481cd18c9b7962023ad794a9086455e3adf68efa
Riankai1073 <riancorci@gmail.com>
2021-10-10 20:11:42 -0700

    Created the AI

---
+++ Battleship/AIPlayer.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   AIPlayer.h
+ * Author: rianc
+ *
+ * Created on October 10, 2021, 6:41 PM
+ */
+
+#ifndef AIPLAYER_H
+#define AIPLAYER_H
+
+
+
+#endif /* AIPLAYER_H */
+
